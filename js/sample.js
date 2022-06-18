da7f5a8acfd67682a20550a0548ed86bb6a0a7b1
Adwasar <Adwasarik@gmail.com>
2022-06-18 20:09:59 +0300

    left-menu sample in JS

--- js/sample.js
+++ js/sample.js
@@ -2,46 +2,47 @@
 const leftMenu = document.querySelector('.leftMenu');
 const leftMenuCountentDeep1 = document.createElement('div');
 
+const javaScript = `JavaScript.html" class="a">JavaScript</a>`,
+      java = `Java.html" class="a">Java</a>`,
+      piton = `Piton.html" class="a">Piton</a>`,
+      php = `PHP.html" class="a">PHP</a>`,
+      cCPlusPlus = `C_C++.html" class="a">C/C++</a>`,
+      cSharp = `C(sharp).html" class="a">C#</a>`;
+
 if (leftMenu.classList.contains('deep1')) {
     leftMenuCountentDeep1.innerHTML = `
     <h3>Специфика</h3>
-        <a href="others/JavaScript.html" class="a">JavaScript</a>
-        <a href="others/Java.html" class="a">Java</a>
-        <a href="others/Piton.html" class="a">Piton</a>
-        <a href="others/PHP.html" class="a">PHP</a>
-        <a href="others/C_C++.html" class="a">C/C++</a>
-        <a href="others/C(sharp).html" class="a">C#</a>
-        <a href="others/C(sharp).html" class="a">test</a>
-        <a href="others/C(sharp).html" class="a">yey</a>
+        <a href="others/${javaScript}
+        <a href="others/${java}
+        <a href="others/${piton}
+        <a href="others/${php}
+        <a href="others/${cCPlusPlus}
+        <a href="others/${cSharp}
     `;
 
-leftMenu.append(leftMenuCountentDeep1);
+    leftMenu.append(leftMenuCountentDeep1);
 } else if (leftMenu.classList.contains('deep2')) {
     leftMenuCountentDeep1.innerHTML = `
     <h3>Специфика</h3>
-        <a href="JavaScript.html" class="a">JavaScript</a>
-        <a href="Java.html" class="a">Java</a>
-        <a href="Piton.html" class="a">Piton</a>
-        <a href="PHP.html" class="a">PHP</a>
-        <a href="C_C++.html" class="a">C/C++</a>
-        <a href="C(sharp).html" class="a">C#</a>
-        <a href="C(sharp).html" class="a">test</a>
-        <a href="C(sharp).html" class="a">yey</a>
+        <a href="${javaScript}
+        <a href="${java}
+        <a href="${piton}
+        <a href="${php}
+        <a href="${cCPlusPlus}
+        <a href="${cSharp}
     `;
     
-leftMenu.append(leftMenuCountentDeep1);
+    leftMenu.append(leftMenuCountentDeep1);
 } else if (leftMenu.classList.contains('deep3')) {
     leftMenuCountentDeep1.innerHTML = `
     <h3>Специфика</h3>
-        <a href="../../JavaScript.html" class="a">JavaScript</a>
-        <a href="../../Java.html" class="a">Java</a>
-        <a href="../../Piton.html" class="a">Piton</a>
-        <a href="../../PHP.html" class="a">PHP</a>
-        <a href="../../C_C++.html" class="a">C/C++</a>
-        <a href="../../C(sharp).html" class="a">C#</a>
-        <a href="../../C(sharp).html" class="a">test</a>
-        <a href="../../C(sharp).html" class="a">yey</a>
+        <a href="../../${javaScript}
+        <a href="../../${java}
+        <a href="../../${piton}
+        <a href="../../${php}
+        <a href="../../${cCPlusPlus}
+        <a href="../../${cSharp}
     `;
 
-leftMenu.append(leftMenuCountentDeep1);
+    leftMenu.append(leftMenuCountentDeep1);
 }
