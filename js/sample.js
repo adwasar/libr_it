f50934a7a0a9a47d5ac6decaeb64216b57e2e17c
Adwasar <Adwasarik@gmail.com>
2022-07-08 09:55:24 +0300

    refatoring main page HTML and JS tabs

--- js/sample.js
+++ js/sample.js
@@ -144,31 +144,30 @@ if (document.querySelector('.compilations') || document.querySelector('.all')) {
 }
 
 // tabs 
+const tabs = document.querySelectorAll('.tabBlock'),
+tabsContent = document.querySelectorAll('[data-tab_content]'),
+tabsParent = document.querySelector('.wrapperTab');
+
+function hideTabsContent () {
+    tabsContent.forEach (item => {
+        item.classList.add('hide');
+        item.classList.remove('tabBlock_active');
+    });
+    tabs.forEach (item => {
+        item.classList.remove('tabBlock_active');
+    });
+}
+
+function showTabsContent (i = 0) {
+    tabsContent[i].classList.add('show');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabBlock_active');
+}
+
 if (document.querySelector('.compilations')) {
-
-    const tabs = document.querySelectorAll('.tabBlock'),
-        tabsContent = document.querySelectorAll('[data-tab_content]'),
-        tabsParent = document.querySelector('.wrapperTab');
-
-    function hideTabsContent () {
-        tabsContent.forEach (item => {
-            item.classList.add('hide');
-            item.classList.remove('tabBlock_active');
-        });
-        tabs.forEach (item => {
-            item.classList.remove('tabBlock_active');
-        });
-    }
-
-    function showTabsContent (i = 0) {
-        tabsContent[i].classList.add('show');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabBlock_active');
-    }
-
     hideTabsContent();
     showTabsContent();
-
+    
     tabsParent.addEventListener('click', (e) => {
         if (e.target.classList.contains('tabBlock')) {
             tabs.forEach ((item, i) => {
@@ -179,5 +178,7 @@ if (document.querySelector('.compilations')) {
             });
         }
     });
-
 }
+
+
+
