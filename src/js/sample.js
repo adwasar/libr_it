6ebb71a0efe81f9223974e492150849665b9a4ec
Adwasar <Adwasarik@gmail.com>
2022-08-04 21:48:44 +0300

    splited module into sample.js

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,20 +1,3 @@
-// leftMenuCountent
-const leftMenu = document.querySelector('.leftMenu');
-
-    leftMenu.innerHTML = `
-    <h3>Навигация</h3>
-        <a href=/libr_it/src/others/javascript.html class="a">JavaScript</a>
-        <a href=/libr_it/src/others/java.html class="a">Java</a>
-        <a href=/libr_it/src/others/python.html class="a">Python</a>
-        <a href=/libr_it/src/others/php.html class="a">PHP</a>
-        <a href=/libr_it/src/others/c_c-plyus.html class="a">C/C++</a>
-        <a href=/libr_it/src/others/c_sharp.html class="a">C#</a>
-        <a href=/libr_it/src/others/web-design.html class="a">Веб-дизайн</a>
-        <a href=/libr_it/src/others/qa.html class="a">QA</a>
-        <a href=/libr_it/src/others/html_css.html class="a">HTML/CSS</a>
-    `;
-    
-
 // Присвоение ноера, для каждого елемента таблицы
 const listItems = document.querySelectorAll("td a");
 
